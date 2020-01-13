b1bd48f85e9db07dac98277824baacb0ecf634e1
AbhiBajpaishek <abhishekbajpai449@gmail.com>
2020-01-13 23:20:24 +0530

    Nothing

--- CoreMvcPuc/Startup.cs
+++ CoreMvcPuc/Startup.cs
@@ -27,7 +27,7 @@ namespace CoreMvcPuc
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
             });
         }
     }
